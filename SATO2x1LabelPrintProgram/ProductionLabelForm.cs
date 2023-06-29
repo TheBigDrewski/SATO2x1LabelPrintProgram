32425b406fefe4dd08d5bce85e7fe7ab7f2ab25b
Andrew Jeschke <64234199+TheBigDrewski@users.noreply.github.com>
2023-06-29 08:18:07 -0500

    Update ProductionLabelForm.cs

--- SATO2x1LabelPrintProgram/ProductionLabelForm.cs
+++ SATO2x1LabelPrintProgram/ProductionLabelForm.cs
@@ -17,45 +17,14 @@ namespace SATO2x1LabelPrintProgram
         private void PrintButton_Click(object sender, EventArgs e)
         {
             //
-            // SATOTest
+            // NewSATO
             //
-            Printer SATOTest = new Printer
+            /*Printer NewSATO = new Printer
             {
                 Interface = Printer.InterfaceType.TCPIP,
                 TCPIPAddress = "10.10.200.82",
                 TCPIPPort = "9100"
-            };
-            //
-            // 199SATOCT40022 - Building 200 - Receiving
-            //
-            Printer _199SATOCT40022 = new Printer
-            {
-                Interface = Printer.InterfaceType.TCPIP,
-                TCPIPAddress = "10.10.199.225",
-                TCPIPPort = "9100",
-                PermanentConnect = true
-            };
-            //199SATOCT4003 - Building 200 - Prep Station 4
-            Printer _199SATOCT4003 = new Printer
-            {
-                Interface = Printer.InterfaceType.TCPIP,
-                TCPIPAddress = "10.10.199.220",
-                TCPIPPort = "9100"
-            };
-            //199SATOCT4004 - Building 200 - Prep Station 2
-            Printer _199SATOCT4004 = new Printer
-            {
-                Interface = Printer.InterfaceType.TCPIP,
-                TCPIPAddress = "10.10.200.27",
-                TCPIPPort = "9100"
-            };
-            //199SATOCT4006 - Building 200 - Prep Station 3
-            Printer _199SATOCT4006 = new Printer
-            {
-                Interface = Printer.InterfaceType.TCPIP,
-                TCPIPAddress = "10.10.200.29",
-                TCPIPPort = "9100"
-            };
+            };*/
 
             string[] data = new string[]
                 {   lotTextBox.Text,
@@ -120,36 +89,11 @@ namespace SATO2x1LabelPrintProgram
                 Success success = new Success();
                 switch (printerComboBox.Text)
                 {
-                    case "199SATOCT4003":
-                        _199SATOCT4003.Connect();
-                        _199SATOCT4003.Query(Encoding.ASCII.GetBytes(sbpl));
+                    case "NewSATO":
+                        NewSATO.Connect();
+                        NewSATO.Query(Encoding.ASCII.GetBytes(sbpl));
                         success.ShowDialog();
-                        _199SATOCT4003.Disconnect();
-                        break;
-
-                    case "199SATOCT4004":
-                        _199SATOCT4004.Connect();
-                        _199SATOCT4004.Query(Encoding.ASCII.GetBytes(sbpl));
-                        success.ShowDialog();
-                        _199SATOCT4004.Disconnect();
-                        break;
-                    case "199SATOCT4006":
-                        _199SATOCT4006.Connect();
-                        _199SATOCT4006.Query(Encoding.ASCII.GetBytes(sbpl));
-                        success.ShowDialog();
-                        _199SATOCT4006.Disconnect();
-                        break;
-                    case "SATOTest":
-                        SATOTest.Connect();
-                        SATOTest.Query(Encoding.ASCII.GetBytes(sbpl));
-                        success.ShowDialog();
-                        SATOTest.Disconnect();
-                        break;
-                    case "199SATOCT40022":
-                        _199SATOCT40022.Connect();
-                        _199SATOCT40022.Query(Encoding.ASCII.GetBytes(sbpl));
-                        success.ShowDialog();
-                        _199SATOCT40022.Disconnect();
+                        NewSATO.Disconnect();
                         break;
                 }
             }
